782cfe03f0021650f63a864b85ad72759f1748b3
thigsbomfim <thigs.bomfim@gmail.com>
2023-02-16 10:51:22 -0300

    configurações

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const app = express();
 
@@ -8,6 +6,7 @@ const conf = require('dotenv').config().parsed;
 const port = conf.HTTPPORT;
 
 // Objeto que será executado quando houver uma requisição.
+
 const controller = {
     resJson: async (req, res) => {
         // Lista com alguns atributos úteis da requisição (req) HTTP
@@ -29,9 +28,10 @@ const controller = {
 
     }
 }
-
 // Recebe os dados do body HTTP e valida em JSON.
 const bodyParser = require('body-parser').json();
+
+/*
 
 // Rota para GET -> getAll() -> Recebe, por exemplo, todos os registros.
 app.get('/', controller.resJson)
@@ -39,10 +39,10 @@ app.get('/', controller.resJson)
 // Rota para GET -> get(id) -> Recebe apenas o registro identificado.
 app.get('/:id', controller.resJson);
 
-// Rota para POST -> post() -> Cria um registro -> bodyParser é usado para garantir a chegada de um JSON. Cria um registro 
+// Rota para POST -> post() -> Cria um registro -> bodyParser (no hook) é usado para garantir a chegada de um JSON.Modifica apenas o registro especifico.
 app.post('/', bodyParser, controller.resJson)
 
-// Rota para PUT -> put() -> Modifica apenas o registro especifico
+// Rota para PUT -> put() -> altera todos os registros -> bodyParser (no hook) é usado para garantir a chegada de um JSON.Modifica apenas o registro especifico.
 app.put('/:id', bodyParser, controller.resJson)
 
 // Rota para DELETE -> delete(id) -> Deleta apenas o registro identificado.
@@ -51,4 +51,90 @@ app.delete('/:id', controller.resJson);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+*/
 
+const array = [];
+
+
+// Objeto que será executado quando houver uma requisição
+const thing = {
+    getAll : async (req, res) => {
+        res.json(
+        { 
+        "resultado": array,
+        "status": "ok"
+        }
+        )
+    },
+
+    getOne: async (req, res) => {
+        const id = req.params.id;
+        res.json(
+        { 
+        "req": req.method, 
+        "id": id, 
+        "status": "ok" 
+        }
+        )
+    },
+
+    post: async (req, res) => {
+        res.json(
+        { 
+        "req": req.method, 
+        "json": req.body,
+        "status": "ok" 
+        }
+        )
+    },
+
+    put: async (req, res) => {
+        const id = req.params.id;
+        res.json(
+        array.push({
+         "req": req.method, 
+         "body": req.body,
+         "id": id,
+         "status": "ok" 
+        })
+        )
+    },
+
+    delete: async (req, res) => {
+        const id = req.params.id;
+        array.push(res.json(
+        { 
+        "req": req.method, 
+        "id": id, 
+        "status": "ok" 
+        })
+        )
+    }
+}
+
+const user = {
+    getOne: async (req, res) => {},
+    post: async (req, res) => {},
+    put: async (req, res) => {},
+    delete: async (req, res) => {}
+}
+
+
+// Rota para coisas
+app.get('/', thing.getAll)
+app.get('/:id', thing.getOne);
+app.post('/', bodyParser, thing.post)
+app.put('/:id', bodyParser, thing.put);
+app.delete('/:id', thing.delete);
+
+
+
+// Rotas para o usuário
+app.get('/user/:id', user.getOne);
+app.put('/user/:id', user.put);
+app.delete('/user/:id', user.delete);
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
