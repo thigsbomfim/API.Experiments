b3b25867de9ef34a063c1ccbd11c39dd23fd4b43
thigsbomfim <thigs.bomfim@gmail.com>
2023-02-17 11:53:46 -0300

    configurando

--- index.js
+++ index.js
@@ -5,28 +5,122 @@ const conf = require('dotenv').config().parsed;
 
 const port = conf.HTTPPORT;
 
-// Objeto que será executado quando houver uma requisição.
+const mysql = require('mysql2');
 
-const controller = {
-    resJson: async (req, res) => {
-        // Lista com alguns atributos úteis da requisição (req) HTTP
-        data = {
-            "method": req.method,
-            "url": req.url,
-            "baseURL": req.baseURL,
-            "query": res.query,
-            "params": req.params,
-            "body": req.body,
-            "headers": req.headers
+const conn = mysql.createPool({
+    host: conf.HOSTNAME,
+    database: conf.DATABASE,
+    user: conf.USERNAME,
+    password: conf.PASSWORD,
+    port: conf.port
+}).promise();
+
+// Objeto que será executado quando houver uma requisição.
+const thing = {
+    getAll : async (req, res) => {
+        try { 
+            // Query que obtém os dados do banco de dados
+            // prepare, sanitização
+            
+            const sql = "SELECT *, DATE_FORMAT(tdate, '%d/%m/%Y às %H:%i') AS tdatebr FROM things WHERE tstatus = 'on' ORDER BY tdate DESC";
+            const [atributos] = await conn.query(sql)
+            
+            // views dos dados
+            res.json({ data: atributos})
+        
         }
 
-        // Envia JSON com os dados acima para o cliente, como texto plano.
-        // res.send(data);
 
-        // Envia JSON com os dados acima para o cliente, como texto plano JSON.
-        res.json(data);
+        catch (error) {
+            res.json({ status: "error", message: error })
+        }
+
+    },
+
+    getOne: async (req, res) => {
+        const id = req.params.id;
+        try { 
+            // Query que obtém os dados do banco de dados
+            // prepare, sanitização
+            
+            const sql = "SELECT *, DATE_FORMAT(tdate, '%d/%m/%Y às %H:%i') AS tdatebr FROM things WHERE tid= ? AND tstatus = 'on' ORDER BY tdate DESC";
+            const [atributos] = await conn.query(sql, [id])
+
+            // views dos dados
+            res.json({ data: atributos})
+        
+        }
+            // Exibe mensagem de erro
+        catch (error) {
+            res.json({ status: "error", message: error })
+        }
+    },
+
+    post: async (req, res) => {
+        try { 
+
+            // Extrai os campos do req.body.
+            const {user, name, photo, description, location, options} = req.body;
+            
+            // Query
+            const sql = "INSERT INTO things( tuser, tname, tphoto, tdescription, tlocation, toptions ) VALUES ( ?, ?, ?, ?, ?, ? );";
+            const [atributos] = await conn.query(sql, [user, name, photo, description, location, options])
+            
+            // Views dos dados
+            res.json({ coisa: atributos.insertId, status: "sucess" })
+        
+        }
+        catch (error) {
+            res.json({ status: "error", message: error })
+        }
+    },
+
+    put: async (req, res) => {
+        const id = req.params.id;
+        try { 
+            // Extrai os campos do req.body.
+            const {user, name, photo, description, location, options} = req.body;
+            
+            // Query
+            const sql = "UPDATE things SET tuser = ?, tname = ?, tphoto = ?, tdescription = ?, tlocation = ?, toptions = ? WHERE tid = ?";
+            const [atributos] = await conn.query(sql,[user, name, photo, description, location, options, id])
+            
+            // Views dos dados
+            res.json({ coisa: atributos.insertId, status: "sucess" })
+        }
+        catch (error) {
+            res.json({ status: "error", message: error })
+        }
+    },
+
+    delete: async (req, res) => {
+        const id = req.params.id;
+        const STATUS_OFF = 'off'
+        try { 
+            // Extrai os campos do req.body.
+            const { status } = req.body;
+
+            // Atualiza o status da coisa com o ID fornecido.
+            const sql = "UPDATE things SET tstatus = ? WHERE tid = ?";
+            const [atributos] = await conn.query(sql,[status || STATUS_OFF, id])
+            
+            // Retorna a resposta com os dados atualizados.
+            res.json({ coisa: id, status: 'success', rowsAffected: result.affectedRows });
+        }
+        catch (error) {
+            console.error(`Erro ao atualizar a coisa ${id}: ${error.message}`);
+            res.status(500).json({ status: 'error', message: 'Erro ao atualizar a coisa.' });
+        }
     }
 }
+
+const user = {
+    getOne: async (req, res) => { },
+    post: async (req, res) => { },
+    put: async (req, res) => { },
+    delete: async (req, res) => { }
+}
+
 
 // Recebe os dados do body HTTP e valida em JSON.
 const bodyParser = require('body-parser').json();
@@ -57,67 +151,6 @@ app.listen(port, () => {
 
 
 // Objeto que será executado quando houver uma requisição
-const thing = {
-    getAll : async (req, res) => {
-        res.json(
-        { 
-        "resultado": array,
-        "status": "ok"
-        }
-        )
-    },
-
-    getOne: async (req, res) => {
-        const id = req.params.id;
-        res.json(
-        { 
-        "req": req.method, 
-        "id": id, 
-        "status": "ok" 
-        }
-        )
-    },
-
-    post: async (req, res) => {
-        res.json(
-        { 
-        "req": req.method, 
-        "json": req.body,
-        "status": "ok" 
-        }
-        )
-    },
-
-    put: async (req, res) => {
-        const id = req.params.id;
-        res.json(
-        {
-         "req": req.method, 
-         "body": req.body,
-         "id": id,
-         "status": "ok" 
-        }
-        )
-    },
-
-    delete: async (req, res) => {
-        const id = req.params.id;
-        res.json(
-        { 
-        "req": req.method, 
-        "id": id, 
-        "status": "ok" 
-        }
-        )
-    }
-}
-
-const user = {
-    getOne: async (req, res) => {},
-    post: async (req, res) => {},
-    put: async (req, res) => {},
-    delete: async (req, res) => {}
-}
 
 
 // Rota para coisas/thing
@@ -132,6 +165,9 @@ app.delete('/:id', thing.delete);
 app.get('/user/:id', user.getOne);
 app.put('/user/:id', user.put);
 app.delete('/user/:id', user.delete);
+
+
+
 
 
 app.listen(port, () => {
